ee2de96f54cf9e24bd41b065c66308e3d60e8778
kieran-mcast <kieran.camilleri.b31252@mcast.edu.mt>
2021-03-18 10:50:48 +0100

    edited profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -5,7 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit
+{
+  // The user's name.
+  public name: string = "Redd";
+
+  // The user's surname.
+  public surname: string = "Caruana";
 
   constructor() { }
 
