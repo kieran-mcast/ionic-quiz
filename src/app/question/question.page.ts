664c2f5e9fb60f764128343554b1cd64865969f5
kieran-mcast <kieran.camilleri.b31252@mcast.edu.mt>
2021-03-24 12:01:55 +0100

    Questions

--- src/app/question/question.page.ts
+++ src/app/question/question.page.ts
@@ -5,11 +5,49 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './question.page.html',
   styleUrls: ['./question.page.scss'],
 })
-export class QuestionPage implements OnInit {
+export class QuestionPage implements OnInit
+{
+  public question: any = {
+    text: "Ionic is a mobile framework based on Angular.",
+    randomize: true,
+    answers: [
+      {
+        text: "True",
+        correct: true
+      },
+      {
+        text: "False"
+      }
+    ]
+  };
+
+  // The answer clicked in the list.
+  // -1 -> no answer has been chosen.
+  public chosenAnswer: number = -1;
+
+  // Whether or not the answer was submitted.
+  public submitted: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * Toggles between submitted and not submitted.
+   */
+  onSubmitAnswer(): void
+  {
+    if (this.submitted)
+    {
+      // change the submitted value to false.
+      this.submitted = false;
+      // reset the chosen answer.
+    }
+    else
+    {
+      // change the submitted value to true.
+      this.submitted = true;
+    }
+  }
 }
